4f17477a9f51a3b6c2b61f97f32b85471decf047
Mary Webby <118230483+marywebby@users.noreply.github.com>
2024-02-13 17:16:49 +0000

    delete instances varibables from create action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -16,15 +16,15 @@ class MoviesController < ApplicationController
   end
 
   def create 
-    @m = Movie.new
-    @m.title = params.fetch("the_title")
-    @m.year = params.fetch("the_year")
-    @m.duration = params.fetch("the_duration")
-    @m.description = params.fetch("the_description")
-    @m.image = params.fetch("the_image")
-    @m.director_id = params.fetch("the_director_id")
+    m = Movie.new
+    m.title = params.fetch("the_title")
+    m.year = params.fetch("the_year")
+    m.duration = params.fetch("the_duration")
+    m.description = params.fetch("the_description")
+    m.image = params.fetch("the_image")
+    m.director_id = params.fetch("the_director_id")
 
-    @m.save
+    m.save
 
     redirect_to("/movies")
   end
@@ -39,6 +39,25 @@ class MoviesController < ApplicationController
     the_movie.destroy
 
     redirect_to("/movies")
+  end
+
+  def update 
+    m_id = params.fetch("the_id")
+
+    matching_records = Movie.where({ :id => m_id })
+
+    the_movie = matching_records.at(0)
+
+    the_movie.title = params.fetch("the_title")
+    the_movie.year = params.fetch("the_year")
+    the_movie.duration= params.fetch("the_duration")
+    the_movie.description = params.fetch("the_description")
+    the_movie.image = params.fetch("the_image")
+    the_movie.director_id = params.fetch("the_director_id")
+
+    the_movie.save
+
+    redirect_to("/movies/#{the_movie.id}")
   end
 
 end
